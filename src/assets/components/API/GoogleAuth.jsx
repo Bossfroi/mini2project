920928f1d0bd3256f0747f7b5634d3d4251c8cd1
Bossfroi <froisidmer@gmail.com>
2023-10-13 15:44:43 +0800

    13/10/23 PM

--- src/assets/components/API/GoogleAuth.jsx
+++ src/assets/components/API/GoogleAuth.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { GoogleLogin } from '@react-oauth/google';
-
+import jwtDecode from 'jwt-decode';
 export default function GoogleAuth() {
   return (
 <>
 
 <GoogleLogin
   onSuccess={credentialResponse => {
-    console.log(credentialResponse);
+    const credentialResponseDecode = jwtDecode(credentialResponse.credential)
+    console.log(credentialResponseDecode);
   }}
   onError={() => {
     console.log('Login Failed');
   }}
-/>;
+/>
 </>
    
   )
