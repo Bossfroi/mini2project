9f29ac7c5fe5c0cbb009dd0b3a38f96369e4b4c0
jnecesario318 <njhonroy@gmail.com>
2023-10-15 04:52:31 +0800

    starting home page

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -4,7 +4,26 @@ import { Outlet } from "react-router-dom";
 export default function Home() {
   return (
     <>
-     <></>
+     <>
+     <div className='relative'>
+      <img 
+      src="/src/assets/components/images/hero.jpg" 
+      alt="" 
+      className='w-full'/>
+      <div className='absolute inset-0 items-center justify-center '>
+      <p className='p-4 md:p-8 lg:p-12 text-3xl md:text-4xl lg:text-5xl font-extrabold text-center leading-snug'>
+        Building Dreams with Quality Rentals.
+      </p>
+      <p className='p-4 md:p-8 lg:p-12 text-3xl md:text-4xl lg:text-5xl font-extrabold text-center leading-snug'>
+      From the foundation of your dreams to the towering 
+heights of your ambitions, our extensive selection of 
+top-quality construction equipment rentals is here to 
+provide you with the tools, support, and reliability you need. 
+We're more than just equipment 
+      </p>
+      </div>
+     </div>
+     </>
     <Outlet/>
     </>
   )
