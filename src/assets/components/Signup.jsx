70594b51d64b4427ad2a46105173a18cf9c15a0b
jnecesario318 <njhonroy@gmail.com>
2023-10-14 17:17:05 +0800

    update sign up page

--- src/assets/components/Signup.jsx
+++ src/assets/components/Signup.jsx
@@ -3,7 +3,45 @@ import React from 'react'
 export default function Signup() {
   return (
     <>
-
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
    </>
   )
 }
