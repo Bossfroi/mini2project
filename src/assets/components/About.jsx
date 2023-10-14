30fc5de1f1ba6c5668e6505944efb7e425c3a228
jnecesario318 <njhonroy@gmail.com>
2023-10-14 18:33:10 +0800

    final sign up form design

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -4,6 +4,45 @@ import Navigation from './Navigation'
 
 export default function About() {
   return (
-   <></>
+   <>
+   <div className='flex items-center justify-center h-screen bg-gray-200'>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div></>
   )
 }
--- src/assets/components/Signup.jsx
+++ src/assets/components/Signup.jsx
@@ -3,45 +3,51 @@ import React from 'react'
 export default function Signup() {
   return (
     <>
-    <div>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
+    <section className=' className="bg-gray-100 flex items-center justify-center mt-2'>
+    <div className='bg-slate-50 p-8 w-96 shadow-md mt-2 justify-center'>
+    <img className="flex-auto " src="/src/assets/components/images/Logo.png" alt="Logo" />
+      <h2 className="text-2xl font-semibold mb-4">Sign Up</h2>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="name" className="block text-dark-600">Name:</label>
           <input
             type="text"
             id="name"
             name="name"
-            value={formData.name}
-            onChange={handleChange}
             required
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
+        <div className="mb-3">
+          <label htmlFor="email" className="block text-dark-600">Email:</label>
           <input
             type="email"
             id="email"
             name="email"
-            value={formData.email}
-            onChange={handleChange}
             required
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
+        <div className="mb-3">
+          <label htmlFor="password" className="block text-dark-600">Password:</label>
           <input
             type="password"
             id="password"
             name="password"
-            value={formData.password}
-            onChange={handleChange}
             required
           />
         </div>
-        <button type="submit">Sign Up</button>
+        <div className="mb-3">
+          <label htmlFor="confirmPassword" className="block text-dark-600">Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            required
+          />
+        </div>
+        <button type="submit" className=" bg-sky-600 shadow-lg py-2 px-4 rounded-md hover:bg-red-700">Sign Up</button>
       </form>
     </div>
+    </section>
    </>
   )
 }
