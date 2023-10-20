4af8bef57525787e3f4dd6b67953fe0b0b9d32c4
Bossfroi <froisidmer@gmail.com>
2023-10-21 07:27:43 +0800

    21-10-23 7:30AM

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -6,31 +6,31 @@ export default function About() {
   return (
    <>
     <body>
-    <div class="relative">
+    <div className="relative">
     <img
       src="/src/assets/components/images/hero1.jpg"
       alt=""
-      class="w-full h-auto filter blur-sm"
+      className="w-full h-auto filter blur-sm"
     />
-      <div class="absolute inset-0 flex items-center justify-center bg-gray-900 bg-opacity-70 text-white">
-        <p class="p-4 md:p-8 lg:p-12 text-3xl md:text-4xl lg:text-5xl font-extrabold text-center leading-snug">
+      <div className="absolute inset-0 flex items-center justify-center bg-gray-900 bg-opacity-70 text-white">
+        <p className="p-4 md:p-8 lg:p-12 text-3xl md:text-4xl lg:text-5xl font-extrabold text-center leading-snug">
           Empowering Construction Excellence Through Premium Rentals Your Trusted Resource for Quality Equipment and Tools. Build Smarter, Build Faster with Our Unmatched Rental Solutions.
         </p>
       </div>
     </div>
     
-    <div class="container mx-auto p-7 bg-red-200">
-    <h1 class="text-3xl font-bold mb-4">Our Core Pillars</h1>
+    <div className="container mx-auto p-7 bg-red-200">
+    <h1 className="text-3xl font-bold mb-4">Our Core Pillars</h1>
 
-    <div class="flex flex-wrap">
-      <div class="w-full md:w-1/2 lg:w-1/3 p-20 bg-red-300">
-        <h2 class="text-xl font-bold">Quality Equipment and Tools:</h2>
+    <div className="flex flex-wrap">
+      <div className="w-full md:w-1/2 lg:w-1/3 p-20 bg-red-300">
+        <h2 className="text-xl font-bold">Quality Equipment and Tools:</h2>
         <p>Offering a comprehensive and high-quality inventory of construction equipment and tools is one of the most critical pillars. This includes construction machinery, power tools, vehicles, safety equipment, and more. Ensuring that the equipment is well-maintained, up-to-date, and reliable is essential for meeting the needs of construction professionals.</p>
       </div>
 
   
-      <div class="w-full md:w-1/2 lg:w-1/3 p-20 mt-4 md:mt-0 bg-red-400">
-        <h2 class="text-xl font-bold">Availability and Accessibility:</h2>
+      <div className="w-full md:w-1/2 lg:w-1/3 p-20 mt-4 md:mt-0 bg-red-400">
+        <h2 className="text-xl font-bold">Availability and Accessibility:</h2>
         <p>Availability and accessibility are key factors in the construction rentals business. Having a wide range of equipment available for rent and ensuring that it is readily accessible to customers is crucial. This includes having multiple rental locations, an efficient reservation and pickup system, and a responsive customer support team to assist with inquiries and bookings.</p>
       </div>
 
